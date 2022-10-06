a4aec357a88419bd5cd3bf0fd9c1e36a238b3612
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-06 20:14:37 +0100

    Created constructor for class Car, creates a random vehicle type with a random amount of fuel proportional to its fuel tank size

--- Program.cs
+++ Program.cs
@@ -10,4 +10,19 @@ class Car
     public int fuellingTime;
     public int waitingTime;
     public bool isFuelling;
+    public Car()
+    {
+        String[] model = { "Car", "Van", "HGV" }; //array of car types available
+        int[] fuelSizes = { 50, 80, 150 };        //array of fuel capacities available
+
+        Random rnd = new Random();
+        int newType = rnd.Next(3); //creates a random value between 0 and 3 (not including 3), will be used to create a car type and ensure it can have correct fuel capacity
+        type = model[newType];
+        maxFuelCapacity = fuelSizes[newType];
+        fuelInTank = rnd.Next(5, maxFuelCapacity / 2); //the fuel in their tank which has a maximum of less than half of the max fuel capacity
+        int temp = Convert.ToInt32(Math.Floor((maxFuelCapacity - fuelInTank) / 1.5)); //ensures that does not round above the max fuel capacity and makes it an integer from double
+        fuellingTime = rnd.Next(0, temp); //the length of time it takes to fuel is found by working out the time it would take to fully fuel and using that as the upper parameter in a Random.Next() method
+        waitingTime = rnd.Next(20, 80);
+        isFuelling = false;
+    }
 }
