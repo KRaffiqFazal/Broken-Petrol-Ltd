b246e91a73117e9124c7b54ad974523e23e48afe
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-06 20:10:52 +0100

    Created Car class and declared variables that each car would require

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Timers;
+
+class Car
+{
+
+    public string type;
+    public int maxFuelCapacity;
+    public int fuelInTank;
+    public int fuellingTime;
+    public int waitingTime;
+    public bool isFuelling;
+}
